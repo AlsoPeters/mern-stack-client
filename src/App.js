f75eeda85157289d2dcc8f97bb98a37d3cdb172a
Kyle Garcia <kyle.fire@gmail.com>
2021-07-08 21:47:54 +0900

    started front end input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+
+  const addFriend = () => {
+    Axios.post('http://localhost:3001/addfriend', { name: name, age: age })
+      .then(() => {
+        console.log('Friend Sent!');
+      })
+      .catch(() => {
+        console.log('Could not add friend.');
+      });
+  };
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='inputs'>
+        <input
+          type='text'
+          placeholder='Friend name...'
+          onChange={(event) => {
+            setName(event.target.value);
+            console.log(name);
+          }}
+        />
+        <input
+          type='number'
+          placeholder='Friend age...'
+          onChange={(event) => {
+            setAge(event.target.value);
+            console.log(age);
+          }}
+        />
+
+        <button onClick={addFriend}>Add Friend</button>
+      </div>
     </div>
   );
 }
